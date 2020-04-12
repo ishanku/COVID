17726e30547d88cd71f50c24f3340c7d6f00ce11
SSiva Raja <ishanku@gmail.com>
2020-04-11 23:23:43 -0400

    big quer check

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from google.cloud import bigquery
+
 app = Flask(__name__)
 
 
@@ -14,6 +16,31 @@ def hello_name(name):
 @app.route('/sql')
 def sqlname():
     return "I am using cloud sql"
+
+@app.route('/bigquery')
+def query_stackoverflow():
+    # [START bigquery_simple_app_client]
+    client = bigquery.Client()
+    # [END bigquery_simple_app_client]
+    # [START bigquery_simple_app_query]
+    query_job = client.query("""
+        SELECT
+          CONCAT(
+            'https://stackoverflow.com/questions/',
+            CAST(id as STRING)) as url,
+          view_count
+        FROM `bigquery-public-data.stackoverflow.posts_questions`
+        WHERE tags like '%google-bigquery%'
+        ORDER BY view_count DESC
+        LIMIT 10""")
+
+    results = query_job.result()  # Waits for job to complete.
+    # [END bigquery_simple_app_query]
+
+    # [START bigquery_simple_app_print]
+    for row in results:
+        print("{} : {} views".format(row.url, row.view_count))
+    # [END bigquery_simple_app_print]
 
 if __name__ == '__main__':
     app.run()
