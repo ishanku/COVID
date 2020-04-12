cd7704708145ca06c28d56f8725f535dd55c3ee7
SSiva Raja <ishanku@gmail.com>
2020-04-11 23:32:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,32 +17,32 @@ def hello_name(name):
 def sqlname():
     return "I am using cloud sql"
 
-@app.route('/bigquery')
-def query_stackoverflow():
-    # [START bigquery_simple_app_client]
-    client = bigquery.Client()
-    # [END bigquery_simple_app_client]
-    # [START bigquery_simple_app_query]
-    query_job = client.query("""
-        SELECT
-          CONCAT(
-            'https://stackoverflow.com/questions/',
-            CAST(id as STRING)) as url,
-          view_count
-        FROM `bigquery-public-data.stackoverflow.posts_questions`
-        WHERE tags like '%google-bigquery%'
-        ORDER BY view_count DESC
-        LIMIT 10""")
+# @app.route('/bigquery')
+# def query_stackoverflow():
+#     # [START bigquery_simple_app_client]
+#     client = bigquery.Client()
+#     # [END bigquery_simple_app_client]
+#     # [START bigquery_simple_app_query]
+#     query_job = client.query("""
+#         SELECT
+#           CONCAT(
+#             'https://stackoverflow.com/questions/',
+#             CAST(id as STRING)) as url,
+#           view_count
+#         FROM `bigquery-public-data.stackoverflow.posts_questions`
+#         WHERE tags like '%google-bigquery%'
+#         ORDER BY view_count DESC
+#         LIMIT 10""")
 
-    results = query_job.result()  # Waits for job to complete.
-    # [END bigquery_simple_app_query]
+#     results = query_job.result()  # Waits for job to complete.
+#     # [END bigquery_simple_app_query]
 
-    # [START bigquery_simple_app_print]
-    resultsr = ""
-    for row in results:
-        #print("{} : {} views".format(row.url, row.view_count))
-        resultsr = resultsr + "br" + "{} : {} views".format(row.url, row.view_count)
-    return resultsr
+#     # [START bigquery_simple_app_print]
+#     resultsr = ""
+#     for row in results:
+#         #print("{} : {} views".format(row.url, row.view_count))
+#         resultsr = resultsr + "br" + "{} : {} views".format(row.url, row.view_count)
+#     return resultsr
     # [END bigquery_simple_app_print]
 
 if __name__ == '__main__':
